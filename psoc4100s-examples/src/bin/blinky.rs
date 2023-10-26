1e34c60d98e40f8e79222f94bb67f8f597081685
Gabriel Smith <ga29smith@gmail.com>
2023-10-26 01:31:16 -0400

    Add blinky exaple for PSoC 4100S

---
+++ psoc4100s-examples/src/bin/blinky.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_time::Timer;
+use psoc4100s_hal::gpio::{Level, Output};
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = psoc4100s_hal::init(Default::default());
+    let mut led = Output::new(p.PIN_1_6, Level::Low);
+
+    loop {
+        info!("led on!");
+        led.set_high();
+        Timer::after_secs(1).await;
+
+        info!("led off!");
+        led.set_low();
+        Timer::after_secs(1).await;
+    }
+}

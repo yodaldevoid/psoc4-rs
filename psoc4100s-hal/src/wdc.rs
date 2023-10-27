c990836fc59c9e12755d67d36f553897f1831f4a
Gabriel Smith <ga29smith@gmail.com>
2023-10-26 21:18:21 -0400

    hal: Rework WDC-based timer driver
    The previous design assumed that the cascaded counters worked in a
    typical and sensible way.
    
    This was not the case.
    
    For the PSoC4100S and PSoC4100S Plus, the WDC counters cascade not on
    rolling over, but on match. This is true no matter if they are in
    clear-on-match or free-run mode. This means that if we want to use the
    counters together as a 64-bit counter, we cannot use the interrupts for
    arbitrary timestamps.
    
    The new design uses the 32-bit free-run counter coupled with an
    AtomicU32 as the 64-bit timestamp source. That leaves the two 16-bit
    counters to act as alarms. Because all WDC counters run off the same
    clock, they will stay in lock-step with the 32-bit counter used to keep
    track of the timestamp.
    
    Both 16-bit counters are set up in free-run mode so they stay "in phase"
    with the 32-bit counter. The 16-bit counters are set to trigger on the
    difference between "now" and the timestamp at the point where the alarm
    was started. Any alarm set more than 1.6 seconds into the future will
    trigger a few times in-between, but that is fine. There will be some
    additionally delay added to the required delay due to setup time. This
    setup time is dominated by required delays for resetting and
    enabling/disabling the timers. These delays are minimized by starting
    these operations as early as feasibly possible; when an alarm is being
    disabled.

--- psoc4100s-hal/src/wdc.rs
+++ psoc4100s-hal/src/wdc.rs
@@ -1,5 +1,5 @@
 use core::cell::Cell;
-use portable_atomic::{AtomicBool, Ordering};
+use portable_atomic::{AtomicU32, AtomicU8, Ordering};
 
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embassy_sync::blocking_mutex::Mutex;
@@ -16,27 +16,30 @@ struct AlarmState {
 }
 unsafe impl Send for AlarmState {}
 
+const ALARM_COUNT: usize = 2;
 const DUMMY_ALARM: AlarmState = AlarmState {
     timestamp: Cell::new(0),
     callback: Cell::new(None),
 };
 
 struct WdcTimerDriver {
-    alarm: Mutex<CriticalSectionRawMutex, AlarmState>,
-    alarm_allocated: AtomicBool,
+    count_high: AtomicU32,
+    alarms: Mutex<CriticalSectionRawMutex, [AlarmState; ALARM_COUNT]>,
+    next_alarm: AtomicU8,
 }
 
 embassy_time::time_driver_impl!(static DRIVER: WdcTimerDriver = WdcTimerDriver {
-    alarm: Mutex::const_new(CriticalSectionRawMutex::new(), DUMMY_ALARM),
-    alarm_allocated: AtomicBool::new(false),
+    count_high: AtomicU32::new(0),
+    alarms: Mutex::const_new(CriticalSectionRawMutex::new(), [DUMMY_ALARM; ALARM_COUNT]),
+    next_alarm: AtomicU8::new(0),
 });
 
 impl Driver for WdcTimerDriver {
     fn now(&self) -> u64 {
         loop {
-            let hi = pac::WCO.wdt_ctrhigh().read();
-            let lo = pac::WCO.wdt_ctrlow().read().0;
-            let hi2 = pac::WCO.wdt_ctrhigh().read();
+            let hi = self.count_high.load(Ordering::Relaxed);
+            let lo = pac::WCO.wdt_ctrhigh().read();
+            let hi2 = self.count_high.load(Ordering::Relaxed);
             if hi == hi2 {
                 return (hi as u64) << 32 | (lo as u64);
             }
@@ -44,47 +47,73 @@ impl Driver for WdcTimerDriver {
     }
 
     unsafe fn allocate_alarm(&self) -> Option<AlarmHandle> {
-        if self.alarm_allocated.swap(true, Ordering::AcqRel) {
-            None
-        } else {
-            Some(AlarmHandle::new(0))
+        let id = self
+            .next_alarm
+            .fetch_update(Ordering::AcqRel, Ordering::Acquire, |x| {
+                if x < ALARM_COUNT as u8 {
+                    Some(x + 1)
+                } else {
+                    None
+                }
+            });
+
+        match id {
+            Ok(id) => Some(AlarmHandle::new(id)),
+            Err(_) => None,
         }
     }
 
-    fn set_alarm_callback(&self, _: AlarmHandle, callback: fn(*mut ()), ctx: *mut ()) {
+    fn set_alarm_callback(&self, alarm: AlarmHandle, callback: fn(*mut ()), ctx: *mut ()) {
+        let n = alarm.id() as usize;
         critical_section::with(|cs| {
-            let alarm = &self.alarm.borrow(cs);
+            let alarm = &self.alarms.borrow(cs)[n];
             alarm.callback.set(Some((callback, ctx)));
         })
     }
 
-    fn set_alarm(&self, _: AlarmHandle, timestamp: u64) -> bool {
+    fn set_alarm(&self, alarm: AlarmHandle, timestamp: u64) -> bool {
+        let n = alarm.id() as usize;
         critical_section::with(|cs| {
-            let alarm = &self.alarm.borrow(cs);
-            alarm.timestamp.set(timestamp);
+            let alarm = &self.alarms.borrow(cs)[n];
 
-            // TODO: if the timestamp is less-than or equal to 3 clock cycles
-            // into the future, the alarm might be missed.
-
-            // Arm it.
-            pac::WCO.wdt_config().modify(|r| {
-                r.set_wdt_mode0(WdtMode::INT);
-                r.set_wdt_mode1(WdtMode::NOTHING);
+            // Disable the counter in case the previous timestamp wasn't reached before the
+            // alarm was changed or if the new timestamp is in the past.
+            self.wait_counter_enable_acknowledged(n);
+            pac::WCO.wdt_control().modify(|r| {
+                r.set_wdt_enable(n, false);
+                r.set_wdt_reset(n, true);
             });
 
             let now = self.now();
             if timestamp <= now {
                 // If the alarm timestamp has passed, the alarm will not fire.
-                // Disarm the alarm and return `false` to indicate that.
-                pac::WCO.wdt_config().modify(|r| {
-                    r.set_wdt_mode0(WdtMode::NOTHING);
-                    r.set_wdt_mode1(WdtMode::NOTHING);
-                });
-
+                // Return `false` to indicate that.
                 alarm.timestamp.set(u64::MAX);
 
                 false
             } else {
+                alarm.timestamp.set(timestamp);
+
+                while pac::WCO.wdt_control().read().wdt_enabled(n) {}
+
+                // Note that we're not checking the high bits at all. This means the IRQ may
+                // fire early if the alarm is more than 1.6 seconds (2^16 / 4e5 Hz) in the
+                // future. This is OK, since on IRQ fire it is checked if the alarm time has
+                // passed.
+                let diff = timestamp - now;
+                pac::WCO
+                    .wdt_match()
+                    .modify(|r| r.set_wdt_match(n, diff as u16));
+                pac::WCO
+                    .wdt_config()
+                    .modify(|r| r.set_wdt_mode(n, WdtMode::INT));
+
+                // We don't want to enable the timer before the previous reset completes.
+                while pac::WCO.wdt_control().read().wdt_reset(n) {}
+
+                self.wait_counter_enable_acknowledged(n);
+                pac::WCO.wdt_control().modify(|r| r.set_wdt_enable(n, true));
+
                 true
             }
         })
@@ -92,51 +121,60 @@ impl Driver for WdcTimerDriver {
 }
 
 impl WdcTimerDriver {
-    fn check_alarm(&self) {
-        critical_section::with(|cs| {
-            let alarm = &self.alarm.borrow(cs);
-            let timestamp = alarm.timestamp.get();
-            if timestamp <= self.now() {
-                // Disarm the alarm.
-                pac::WCO.wdt_config().modify(|r| {
-                    r.set_wdt_mode0(WdtMode::NOTHING);
-                    r.set_wdt_mode1(WdtMode::NOTHING);
-                });
+    fn check_alarm(&self, n: usize) {
+        if pac::WCO.wdt_control().read().wdt_int(n) {
+            critical_section::with(|cs| {
+                let alarm = &self.alarms.borrow(cs)[n];
+                let timestamp = alarm.timestamp.get();
+                if timestamp <= self.now() {
+                    // Disarm the alarm.
+                    pac::WCO.wdt_match().modify(|r| r.set_wdt_match(n, 0xFFFF));
+                    pac::WCO.wdt_control().modify(|r| {
+                        r.set_wdt_enable(n, false);
+                        // Reset the timer for the next alarm.
+                        r.set_wdt_reset(n, true);
+                    });
 
-                alarm.timestamp.set(u64::MAX);
+                    alarm.timestamp.set(0);
 
-                // Call after clearing alarm so the callback can set another
-                // alarm.
-                if let Some((f, ctx)) = alarm.callback.get() {
-                    f(ctx);
-                }
-            } else {
-                // If we have not reached the timestamp, set the next alarm and
-                // continue waiting.
-                pac::WCO.wdt_config().modify(|r| {
-                    if r.wdt_mode0() == WdtMode::NOTHING {
-                        r.set_wdt_mode0(WdtMode::INT);
-                        r.set_wdt_mode1(WdtMode::NOTHING);
-                    } else {
-                        r.set_wdt_mode0(WdtMode::NOTHING);
-                        r.set_wdt_mode1(WdtMode::INT);
+                    // Call after clearing alarm so the callback can set another alarm.
+                    if let Some((f, ctx)) = alarm.callback.get() {
+                        f(ctx);
                     }
-                });
-            }
-        });
+                }
+            });
 
-        // Clear the IRQ.
-        pac::WCO.wdt_control().modify(|r| {
-            r.set_wdt_int0(true);
-            r.set_wdt_int1(true);
-        });
-        pac::WCO.wdt_control().read();
+            // Clear the IRQ.
+            pac::WCO.wdt_control().modify(|r| r.set_wdt_int(n, true));
+            pac::WCO.wdt_control().read();
+        }
+    }
+
+    fn check_timer(&self) {
+        if pac::WCO.wdt_control().read().wdt_int(2) {
+            self.count_high.fetch_add(1, Ordering::AcqRel);
+
+            // Clear the IRQ.
+            pac::WCO.wdt_control().modify(|r| r.set_wdt_int(2, true));
+            pac::WCO.wdt_control().read();
+        }
+    }
+
+    fn wait_counter_enable_acknowledged(&self, n: usize) {
+        loop {
+            let r = pac::WCO.wdt_control().read();
+            if r.wdt_enable(n) == r.wdt_enabled(n) {
+                break;
+            }
+        }
     }
 }
 
 pub(crate) unsafe fn init() {
     critical_section::with(|cs| {
-        DRIVER.alarm.borrow(cs).timestamp.set(u64::MAX);
+        for n in 0..ALARM_COUNT {
+            DRIVER.alarms.borrow(cs)[n].timestamp.set(u64::MAX);
+        }
     });
 
     pac::WCO.wdt_clken().write(|r| {
@@ -145,14 +183,16 @@ pub(crate) unsafe fn init() {
     });
 
     pac::WCO.wdt_match().write(|r| {
-        r.set_wdt_match0(0xFFFF);
-        r.set_wdt_match1(0xFFFF);
+        r.set_wdt_match(0, 0xFFFF);
+        r.set_wdt_match(1, 0xFFFF);
     });
 
     let use_wco = cfg!(feature = "time-driver-wdc-wco");
     pac::WCO.wdt_config().write(|r| {
-        r.set_wdt_cascade0_1(true);
-        r.set_wdt_cascade1_2(true);
+        r.set_wdt_mode(0, WdtMode::INT);
+        r.set_wdt_mode(1, WdtMode::INT);
+        r.set_wdt_mode2(WdtMode2::INT);
+        r.set_wdt_bits2(31);
         r.set_lfclk_sel(use_wco as u8);
     });
 
@@ -169,17 +209,8 @@ pub(crate) unsafe fn init() {
     };
     wait_cycles(sysclk_cycles);
 
-    pac::WCO.wdt_control().write(|r| {
-        r.set_wdt_enable0(true);
-        r.set_wdt_enable1(true);
-        r.set_wdt_enable2(true);
-    });
-    loop {
-        let r = pac::WCO.wdt_control().read();
-        if r.wdt_enabled0() && r.wdt_enabled1() && r.wdt_enabled2() {
-            break;
-        }
-    }
+    pac::WCO.wdt_control().write(|r| r.set_wdt_enable(2, true));
+    while !pac::WCO.wdt_control().read().wdt_enabled(2) {}
 
     interrupt::WCO_INTERRUPT.enable();
 }
@@ -187,5 +218,8 @@ pub(crate) unsafe fn init() {
 #[cfg(feature = "rt")]
 #[interrupt]
 fn WCO_INTERRUPT() {
-    DRIVER.check_alarm()
+    for n in 0..ALARM_COUNT {
+        DRIVER.check_alarm(n);
+    }
+    DRIVER.check_timer();
 }
